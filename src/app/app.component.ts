d055d768dc7f1d57b6e9e577d0eab1531c83f06e
Fernando Ancajima <fernando.ancajima99@gmail.com>
2024-04-04 10:20:29 -0500

    CarruselEjemplo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,50 @@
-import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import Swiper from 'swiper';
+import { EquiposService } from './services/equipos.service';
+import { register } from 'swiper/element/bundle';
+register();
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
 export class AppComponent {
   title = 'CarruselWeekly';
+  @ViewChild('swiperContainer') swiperContainer!: ElementRef;
+  swiper!: Swiper;
+  equiposList: any = [];
+
+  constructor(
+    private svcEquipos: EquiposService
+  ) {
+    this.svcEquipos.getAllEquipos().subscribe((data: any) => {
+      this.equiposList= data;
+    });
+  }
+
+  ngAfterViewInit(){
+    this.swiper = new Swiper(this.swiperContainer.nativeElement, {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      loop: false,
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20
+        },
+        1024: {
+          slidesPerView: 2,
+          spaceBetween: 50
+        }
+      }
+    });
+  }
 }
